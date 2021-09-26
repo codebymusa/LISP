7297ce4534fc3a483661e29d66c9c0e875d827e1
mmashaba <mmashaba@student.wethinkcode.co.za>
2021-09-26 17:18:52 +0200

    Main method

--- src/main/java/lisp.java
+++ src/main/java/lisp.java
@@ -1,5 +1,11 @@
+import java.io.InputStream;
+import java.util.Scanner;
 
-
-class lisp{
-    
+class Lisp {
+    public static void main(String args[]) {
+        System.out.print(">>> ");
+        Scanner scanner = new Scanner(System.in);
+        String sc = scanner.next();
+        scanner.close();
+    }
 }
